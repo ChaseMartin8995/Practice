fb564c7bc412fb6c9c953a6800d56ad523151950
Chase Martin <cman776@gmail.com>
2018-08-27 20:14:03 -0500

    AWS Update 1

--- request/app.py
+++ request/app.py
@@ -1,5 +1,5 @@
 import os
-import sql
+from sqlalchemy import (create_engine, Table, Column, Integer, String, MetaData, ForeignKey)
 from flask import Flask, render_template, request, json
 app = Flask(__name__)
 app.config['REQUEST_MAIN'] = 'sqlite:////tmp/test.db'
@@ -15,10 +15,33 @@ def main():
 # Pass to SQL
 def mainRequest():
     theData = request.get_json(force=True)
-    sql.insertRequest(theData)
+    insertRequest(theData)
 
     msg = ["Data submitted"]
     return json.dumps(msg)
 
 if __name__=="__main__":
     app.run()
+
+def insertRequest(data):   
+    # kickoff engine
+    engine = create_engine('sqlite:///:memory:', echo=True)
+    metadata = MetaData()
+
+    # create table
+    requests = Table('requests', metadata,
+        Column('title', String(200)),
+        Column('desc', String(2000)),
+        Column('clientid', String(100)),
+        Column('priority', Integer),
+        Column('tdate', String(14)),
+        Column('parea', String(100))
+    )
+
+    metadata.create_all(engine)
+
+    # insert new row
+    theinsert = requests.insert().values(title=data['title'], desc=data['desc'], clientid=data['clientid'], priority=data['priority'], tdate=data['tdate'], parea=data['parea'])
+
+    conn = engine.connect()
+    conn.execute(theinsert)
---
+++ request/rds_config.py
@@ -0,0 +1,4 @@
+rds_host  = "requestdb.ckn7tlauelat.us-east-2.rds.amazonaws.com"
+db_username = "sqladmin"
+db_password = "Orion776"
+db_name = "RequestDB"
