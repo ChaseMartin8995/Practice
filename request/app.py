b712ebe41eb8f040b9d916bf82423d75e4a68b8e
Chase Martin <cman776@gmail.com>
2019-01-21 12:38:30 -0600

    Large practice update
    Large practice update

--- request/app.py
+++ request/app.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import (create_engine, Table, Column, Integer, String, MetaData, ForeignKey)
+import sql
 from flask import Flask, render_template, request, json
 app = Flask(__name__)
 app.config['REQUEST_MAIN'] = 'sqlite:////tmp/test.db'
@@ -10,38 +10,15 @@ def main():
     return render_template('/main.html')
 
 # Handle request
-@app.route('/mainRequest', methods=['POST'])
+@app.route('/mainRequest', methods=['GET','POST'])
 
 # Pass to SQL
 def mainRequest():
     theData = request.get_json(force=True)
-    insertRequest(theData)
+    sql.insertRequest(theData)
 
     msg = ["Data submitted"]
     return json.dumps(msg)
 
 if __name__=="__main__":
     app.run()
-
-def insertRequest(data):   
-    # kickoff engine
-    engine = create_engine('sqlite:///:memory:', echo=True)
-    metadata = MetaData()
-
-    # create table
-    requests = Table('requests', metadata,
-        Column('title', String(200)),
-        Column('desc', String(2000)),
-        Column('clientid', String(100)),
-        Column('priority', Integer),
-        Column('tdate', String(14)),
-        Column('parea', String(100))
-    )
-
-    metadata.create_all(engine)
-
-    # insert new row
-    theinsert = requests.insert().values(title=data['title'], desc=data['desc'], clientid=data['clientid'], priority=data['priority'], tdate=data['tdate'], parea=data['parea'])
-
-    conn = engine.connect()
-    conn.execute(theinsert)
--- request/lambda/app.py
+++ request/lambda/app.py
@@ -9,26 +9,18 @@ name = rds_config.db_username
 password = rds_config.db_password
 db_name = rds_config.db_name
 
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-try:
-    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
-except:
-    logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
-    sys.exit()
+conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=10)
+
 
 logger.info("SUCCESS: Connection to RDS mysql instance succeeded")
 def handler(event, context):
-    """
-    This function fetches content from mysql RDS instance
-    """
 
     item_count = 0
 
     with conn.cursor() as cur:
-        cur.execute("create table Employee3 ( EmpID  int NOT NULL, Name varchar(255) NOT NULL, PRIMARY KEY (EmpID))")  
         cur.execute('insert into Employee3 (EmpID, Name) values(1, "Joe")')
         cur.execute('insert into Employee3 (EmpID, Name) values(2, "Bob")')
         cur.execute('insert into Employee3 (EmpID, Name) values(3, "Mary")')
@@ -38,6 +30,22 @@ def handler(event, context):
             item_count += 1
             logger.info(row)
             #print(row)            
-        conn.commit()
-
+    conn.commit()
+ 
     return "Added %d items from RDS MySQL table" %(item_count)
+
+def insertRequest(data):   
+    # kickoff engine
+    engine = create_engine('requestdb.ckn7tlauelat.us-east-2.rds.amazonaws.com', echo=True)
+    metadata = MetaData()    
+
+    # create table
+    requests = Column('title', String(200)), Column('desc', String(2000)), Column('clientid', String(100)), Column('priority', Integer), Column('tdate', String(14)), Column('parea', String(100)))
+
+    metadata.create_all(engine)
+
+    # insert new row
+    theinsert = requests.insert().values(title=data['title'], desc=data['desc'], clientid=data['clientid'], priority=data['priority'], tdate=data['tdate'], parea=data['parea'])
+
+    conn = engine.connect()
+    conn.commit(theinsert)
